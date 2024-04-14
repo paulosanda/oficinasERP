923694e74fd38fe102addac485565d43d1df4fd3
paulosanda <paulosanda@gmail.com>
2024-04-13 21:57:47 -0300

    feat/roles

---
+++ tests/Feature/RoleControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class RoleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private User $user;
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('db:seed', [RoleSeeder::class]);
+
+        $this->user = User::factory()->create();
+    }
+
+    public function testCompanyRulesIndex(): void
+    {
+        $token = $this->user->createToken('teste', ['root'])->plainTextToken;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson(route('admin.company.rules.index'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'roles' => [[
+            'id',
+            'role'
+        ]]]);
+    }
+
+    public function testCompanyRUlesIndexInvalidAbility(): void
+    {
+        $token = $this->user->createToken('teste', ['master'])->plainTextToken;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson(route('admin.company.rules.index'));
+
+        $response->assertStatus(403);
+
+        $response->assertJson([
+            'message' => 'Invalid ability provided.'
+        ]);
+
+    }
+}
