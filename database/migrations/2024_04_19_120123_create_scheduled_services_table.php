e68a05bd7be8db70a64d8cb88175bb43b97ddb41
paulosanda <paulosanda@gmail.com>
2024-04-19 18:26:24 -0300

    feat/servicos_agendados

---
+++ database/migrations/2024_04_19_120123_create_scheduled_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scheduled_services', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->foreignId('customer_id')->constrained('customer');
+            $table->string('servico');
+            $table->date('data_prevista', 'Y-m-d');
+            $table->date('data_realizado', 'y-m-d')->nullable();
+            $table->boolean('lembrete_ativo')->default(true);
+            $table->string('observacao')->nullable();
+            $table->string('resposta')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scheduled_services');
+    }
+};
